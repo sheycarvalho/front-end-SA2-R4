70531554040c2f6193a9736c33a5bd48ff741d86
sheyenne de morais carvalho <sheyenne_22@hotmail.com>
2022-02-08 18:57:13 -0400

    codificacao do site - parte 4

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    $("#barras").click(function(){
+        $("#menu").toggleClass("#menu-ativo")
+})
+})
+
+// $(document).ready(function(){
+//    $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+
+// })
+
+// setInterval(slide,2000)
+
+// function slide(){
+
+//    if($(".banner-ativo").next().lenght){
+//        $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+//    }else{
+//        $(".banner-ativo").removeClass().hide()
+//        $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+//    }
+// }
+
+//    $("#barras").click(function(){
+//       if($("#menu").hasClass("#menu-ativo")){
+//            $("menu").removeClass("#menu-ativo")
+//        }else{
+//            $("#menu").addClass("menu-ativo")
+//        }
+//    })
+// })
+
+// function mostrarPopup(){
+//    window.alert("Hello word")
+// }
+
+let search = document.getElementById("campo-search");
